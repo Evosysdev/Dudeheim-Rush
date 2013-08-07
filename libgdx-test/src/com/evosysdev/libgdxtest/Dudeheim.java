1d876d4036eb5213a87eaf109b8d5e48f30db6d0
Greg <imperatorfeles@gmail.com>
2013-08-07 13:01:20 -0400

    Added physics

--- libgdx-test/src/com/evosysdev/libgdxtest/Dudeheim.java
+++ libgdx-test/src/com/evosysdev/libgdxtest/Dudeheim.java
@@ -3,16 +3,64 @@ package com.evosysdev.libgdxtest;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class Dudeheim 
 {
 	private Rectangle bound;
 	private Vector2 pos;
+	private Body body;
 	
-	public Dudeheim(Vector2 pos)
+	public Dudeheim(Vector2 pos, float size)
 	{
 		this.pos = pos;
-		this.bound = new Rectangle(pos.x, pos.y, 64, 64);
+		this.bound = new Rectangle(pos.x, pos.y, size, size);
+	}
+	
+	public void createBody(World world)
+	{
+		
+		BodyDef bodyDef = new BodyDef();
+		
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(pos.x * MainScreen.WORLD_TO_BOX, pos.y * MainScreen.WORLD_TO_BOX);
+		
+		body = world.createBody(bodyDef);
+		
+		PolygonShape box = new PolygonShape();
+		box.setAsBox(bound.getWidth() / 2 * MainScreen.WORLD_TO_BOX, bound.getHeight() / 2 * MainScreen.WORLD_TO_BOX);
+		
+		FixtureDef fixture = new FixtureDef();
+		fixture.shape = box;
+		fixture.density = 0.1f;
+		fixture.friction = 0.0f;
+		fixture.restitution = 1.5f;
+		
+		body.createFixture(fixture);
+		
+		body.setAwake(true);
+		body.applyForceToCenter((float)Math.random() * 100 - 50f, (float)Math.random() * 100 - 50f);
+		
+		box.dispose();
+	}
+	
+	public void update()
+	{
+		pos.x = body.getPosition().x * MainScreen.BOX_TO_WORLD;
+		bound.x = body.getPosition().x * MainScreen.BOX_TO_WORLD;
+		pos.y = body.getPosition().y * MainScreen.BOX_TO_WORLD;
+		bound.y = body.getPosition().y * MainScreen.BOX_TO_WORLD;
+	}
+	
+	public Body getBody()
+	{
+		return body;
 	}
 	
 	public void addX(float amt)
@@ -25,12 +73,14 @@ public class Dudeheim
 	{
 		pos.x = amt;
 		bound.x = amt;
+		body.getPosition().x = amt * MainScreen.WORLD_TO_BOX;
 	}
 	
 	public void setY(float y)
 	{
 		pos.y = y;
 		bound.y = y;
+		body.getPosition().y = y * MainScreen.WORLD_TO_BOX;
 	}
 	
 	public float getX()
@@ -50,6 +100,6 @@ public class Dudeheim
 	
 	public boolean isIn(Vector2 point)
 	{
-		return bound.contains(point.x, Gdx.graphics.getHeight() - point.y);
+		return body.getFixtureList().get(0).testPoint(point.x * MainScreen.WORLD_TO_BOX, (Gdx.graphics.getHeight() - point.y) * MainScreen.WORLD_TO_BOX);
 	}
 }
--- libgdx-test/src/com/evosysdev/libgdxtest/MainScreen.java
+++ libgdx-test/src/com/evosysdev/libgdxtest/MainScreen.java
@@ -28,6 +28,15 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeBitm
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -47,24 +56,24 @@ public class MainScreen implements Screen
 	private OrthographicCamera camera;
 	private BitmapFont font;
 	private Sound bleep;
-	private Stage stage;
-	private Skin skin;
-	private TweenManager tweens;
+	private World world;
+	private Box2DDebugRenderer debugRenderer;
 	
 	private int width = Gdx.graphics.getWidth();
 	private int height = Gdx.graphics.getHeight();
 	private float theta;
 	private float time;
 	
+	public static final float WORLD_TO_BOX = 0.01f;
+	public static final float BOX_TO_WORLD = 100;
+	
 	public MainScreen()
 	{
-		skin = new Skin();
-		stage = new Stage();
-		tweens = new TweenManager();
+		world = new World(new Vector2(0.0f, 0.0f), true);
+		
+		debugRenderer = new Box2DDebugRenderer();
 		
 		Tween.registerAccessor(Dudeheim.class, new DudeheimAccessor());
-		
-		Gdx.input.setInputProcessor(stage);
 		
 		Pixmap dudepix = new Pixmap(8, 8, Format.RGBA8888);
 		dudepix.setColor(Color.CYAN);
@@ -76,33 +85,12 @@ public class MainScreen implements Screen
 		dudepix.drawLine(2, 5, 5, 5);
 		dudeheimTex = new Texture(dudepix);
 		
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("data/Amble-Regular.ttf"));
-		font = generator.generateFont(14);
-		generator.dispose();
-		
-		Pixmap pixmap = new Pixmap(64, 32, Format.RGBA8888);
-		pixmap.setColor(Color.WHITE);
-		pixmap.fill();
-		pixmap.setColor(Color.WHITE);
-		pixmap.fillRectangle(0, 0, 64, 32);
-		pixmap.setColor(Color.BLACK);
-		pixmap.fillRectangle(5, 5, 54, 22);
-		skin.add("button", new Texture(pixmap));
-		skin.add("default", new BitmapFont());
-		
-		TextButtonStyle textButtonStyle = new TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("button", Color.DARK_GRAY);
-		textButtonStyle.down = skin.newDrawable("button", Color.DARK_GRAY);
-		textButtonStyle.checked = skin.newDrawable("button", Color.BLUE);
-		textButtonStyle.over = skin.newDrawable("button", Color.LIGHT_GRAY);
-		textButtonStyle.font = skin.getFont("default");
-		skin.add("default", textButtonStyle);
+		font = new BitmapFont();
 		
 		// Create a table that fills the screen. Everything else will go inside this table.
 		Table table = new Table();
 		table.setFillParent(true);
 		table.debug();
-		stage.addActor(table);
 		
 		//dudeheimTex = new Texture(Gdx.files.internal("data/dudeheim.png"));
 		sprites = new SpriteBatch();
@@ -120,17 +108,34 @@ public class MainScreen implements Screen
 
 		camera = new OrthographicCamera(width, height);
 		camera.position.set(width / 2, height / 2, 0);
+		
+		Vector2[] vecs = new Vector2[4];
+		
+		vecs[0] = new Vector2(0, 0);
+		vecs[1] = new Vector2(camera.viewportWidth * WORLD_TO_BOX, 0);
+		vecs[2] = new Vector2(camera.viewportWidth * WORLD_TO_BOX, camera.viewportHeight * WORLD_TO_BOX);
+		vecs[3] = new Vector2(0, camera.viewportHeight * WORLD_TO_BOX);
+		
+		BodyDef groundBodyDef = new BodyDef(); 
+		groundBodyDef.position.set(new Vector2(0, 0));  
+
+		Body groundBody = world.createBody(groundBodyDef);  
+
+		ChainShape border = new ChainShape();  
+		border.createLoop(vecs);
+		groundBody.createFixture(border, 0.0f); 
+		border.dispose(); 
 	}
 	
 	private void addDudes()
 	{
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 200; i++)
 		{
-			Dudeheim heim = new Dudeheim(new Vector2((float)Math.floor(Math.random() * Gdx.graphics.getWidth()),
-					  (float)Math.floor(Math.random() * (Gdx.graphics.getHeight() - 70))));
+			Dudeheim heim = new Dudeheim(new Vector2((float)Math.floor(Math.random() * (Gdx.graphics.getWidth() - 50) + 25),
+					  (float)Math.floor(Math.random() * (Gdx.graphics.getHeight() - 50) + 25)), 32);
 			
 			dudes.add(heim);
-			Tween.to(heim, DudeheimAccessor.POSITION_XY, 1).target(0, 0).ease(Bounce.OUT).start(tweens);
+			heim.createBody(world);
 		}
 	}
 	
@@ -140,34 +145,31 @@ public class MainScreen implements Screen
 		Gdx.gl.glClearColor(1, 0.5f, 0, 1.0f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		//stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-		//stage.draw();
-		//Table.drawDebug(stage);
-		
+		world.step(delta, 6, 2);
 		
 		camera.update();
-		tweens.update(delta);
-		
-		//theta += delta * (100 + (100 - dudes.size()) * 8);
 		
 		
 		for (Dudeheim dude : dudes)
 		{
-			/*
-			dude.addX(delta * (100 + (100 - dudes.size()) * 8));
+			dude.update();
 			
-			if (dude.getPos().x >= Gdx.graphics.getWidth() + 70)
+			if (dude.getX() < 0)
 			{
-				dude.setX(-70);
-			}*/
-			
+				dude.setX(Gdx.graphics.getWidth() - 10);
+			}
+			else if (dude.getX() > Gdx.graphics.getWidth())
+			{
+				dude.setX(10);
+			}
+				
 			sprites.setProjectionMatrix(camera.combined);
 			sprites.begin();
 			font.setColor(0, 0, 0, 1);
 			font.setScale(3);
 			font.draw(sprites, Integer.toString(dudes.size()), 50, 50);
-			sprites.draw(dudeheimTex, dude.getPos().x, dude.getPos().y, 4, 4,
-					dudeheimTex.getWidth(), dudeheimTex.getHeight(), 8, 8, theta, 0, 0, 8, 8, false, false);
+			sprites.draw(dudeheimTex, dude.getX() - 4, dude.getY() - 4, 4, 4, 8, 8, 4, 4,
+					(float)Math.toDegrees(dude.getBody().getAngle()), 0, 0, 8, 8, false, false);
 			sprites.end();
 			
 		}
@@ -178,7 +180,7 @@ public class MainScreen implements Screen
 			{
 				if (dudes.get(i).isIn(new Vector2(Gdx.input.getX(), Gdx.input.getY())))
 				{
-					dudes.remove(i);
+					world.destroyBody(dudes.remove(i).getBody());
 					bleep.play(0.5f);
 				}
 			}
